dd5ccc48ad990ed39e4c5b31840bff2dd0a7e29a
ismael <spacingismael@gmail.com>
2024-10-10 19:42:55 +0100

    task 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""  Duck typing - first element of a sequence """
+import typing as t
+
+
+def safe_first_element(lst: t.Sequence[t.Any]) -> t.Union[t.Any, None]:
+    """ function returns either index 0 of lst, else None"""
+    if lst:
+        return lst[0]
+    else:
+        return None
