752b6fe817b44d5fa62d7a3778796bd5c42167a6
ismael <spacingismael@gmail.com>
2024-10-14 23:38:37 +0100

    fixing task 4 & updating code

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Task"""
 
-
 import asyncio
 import typing as t
 
@@ -9,17 +8,10 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> t.List[float]:
-    """task_wait_n - return the list of all the delays (float values)"""
-    arr: t.List[float] = []
-    var: float
-    while n:
-        ret = await task_wait_random(max_delay)
-        arr.append(ret)
-        n -= 1
-    for i in range(len(arr)):
-        for i in range(len(arr)-1):
-            if (arr[i] > arr[i + 1]):
-                var = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = var
-    return arr
+    """task_wait_n - return the list of all the delays (float values)
+    updated code"""
+    arr = [task_wait_random(max_delay) for _ in range(n)]
+    iterator = asyncio.as_completed(arr)
+    array = [await a for a in iterator]
+
+    return array
