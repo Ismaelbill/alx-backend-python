5f092e31bbeb4d0cfeaa54e4d5c8650363be8cb7
ismael <spacingismael@gmail.com>
2024-10-14 23:34:15 +0100

    fixing & updating task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,17 +8,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> t.List[float]:
-    """wait_n - return the list of all the delays (float values)"""
-    arr: t.List[float] = []
-    var: float
-    while n:
-        ret = await wait_random(max_delay)
-        arr.append(ret)
-        n -= 1
-    for i in range(len(arr)):
-        for i in range(len(arr)-1):
-            if (arr[i] > arr[i + 1]):
-                var = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = var
-    return arr
+    """wait_n - return the list of all the delays (float values)
+    updated code: """
+    arr = [wait_random(max_delay) for _ in range(n)]
+    iterator = asyncio.as_completed(arr)
+    array = [await a for a in iterator]
+
+    return array
