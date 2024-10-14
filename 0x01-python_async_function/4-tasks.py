76d7ec275e7d0b61633e605e743606702fde1d06
ismael <spacingismael@gmail.com>
2024-10-14 13:24:03 +0100

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Task"""
+
+import asyncio
+import typing as t
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> t.List[float]:
+    """task_wait_n - """
+    arr: t.List[float] = []
+    var: float
+    while n:
+        ret = await task_wait_random(max_delay)
+        arr.append(ret)
+        n -= 1
+    for i in range(len(arr)):
+        for i in range(len(arr)-1):
+            if (arr[i] > arr[i + 1]):
+                var = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = var
+    return arr
