32a573c1c1ef07ba7d542152234e04919ad6fdc5
ismael <spacingismael@gmail.com>
2024-10-15 09:50:43 +0100

    task 1, 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+""" Async Comprehensions """
+
+import typing as t
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> t.List[float]:
+    """ collecting 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers. """
+    arr: t.List[float] = [a async for a in async_generator()]
+    return arr
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+""" calculating time it takes to run 4 coroutines async """
+
+import time as tm
+import asyncio as asy
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ measuring the total runtime and return it """
+    start: float = tm.time()
+    await asy.gather(async_comprehension(), async_comprehension(),
+                     async_comprehension(), async_comprehension())
+    end: float = tm.time()
+    return (end - start)
