c801df420c7624dad556c6c4199a447ffd65ad2c
ismael <spacingismael@gmail.com>
2024-10-16 08:29:40 +0100

    fixing task 0 in async comp

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 import typing as t
 
 
-async def async_generator() -> t.AsyncGenerator[float, None]:
+async def async_generator() -> t.Generator[float, None, None]:
     """ coroutine (async_generator) takes no argument
     loops 10 times, waits 1 second, then yield a random number"""
     for _ in range(10):
